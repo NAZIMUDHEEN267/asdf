ef623bd440a99ebccee1e4e17ff7a74fc3c95ccd
NAZIMUDHEEN TI <71973283+NAZIMUDHEEN267@users.noreply.github.com>
2022-05-11 20:56:18 +0530

    as

--- script.js
+++ script.js
@@ -152,3 +152,11 @@ closeBtn.addEventListener("click", () => {
 // service worker registration
 window.navigator.serviceWorker.register("./sw.js");
 
+
+window.addEventListener("beforeinstallprompt", function(e) {
+  // log the platforms provided as options in an install prompt
+  console.log(e.platforms); // e.g., ["web", "android", "windows"]
+  e.userChoice.then(function(choiceResult) {
+    console.log(choiceResult.outcome); // either "accepted" or "dismissed"
+  }, handleError);
+});
